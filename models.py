4866681d1c617041bd9976452ac9e5fa09d9f84c
T3mq <78566312+t3mq@users.noreply.github.com>
2026-03-11 18:09:54 +0100

    update

--- models.py
+++
@@ -1,5 +0,0 @@
-# Ici on liste les personnages dans la collection personnages en affichant ces stats
-def list_characters(characters):
-    for i, character in enumerate(characters, 1):
-        print(f"{i}. {character['nom']} | ATK: {character['atk']} | DEF: {character['def']} | PV: {character['pv']}")
-    return [character['nom'] for character in characters]
